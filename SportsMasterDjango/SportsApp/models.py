3376226a67dc73adda9f48e83871b3a519fb98f5
Marios Georgopetreas <95870493+mgptreas@users.noreply.github.com>
2024-05-27 11:53:07 +0300

    Django no Data

--- SportsMasterDjango/SportsApp/models.py
+++ SportsMasterDjango/SportsApp/models.py
@@ -8,11 +8,15 @@ class Users(models.Model):
     weight = models.FloatField(blank=True, null=True)
     points = models.IntegerField(blank=True, null=True)
     premium = models.BooleanField(default=False)
+    class Meta:
+        db_table = 'Users'  # Specify the table name explicitly
 
 class UserAuth(models.Model):
     user = models.OneToOneField(Users, on_delete=models.CASCADE, primary_key=True)
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    class Meta:
+        db_table = 'UserAuth'  # Specify the table name explicitly
 
 class Sports(models.Model):
     name = models.CharField(max_length=255)
@@ -21,6 +25,8 @@ class Sports(models.Model):
     field3 = models.CharField(max_length=255, blank=True, null=True)
     field4 = models.CharField(max_length=255, blank=True, null=True)
     field5 = models.CharField(max_length=255, blank=True, null=True)
+    class Meta:
+        db_table = 'Sports'  # Specify the table name explicitly
 
 class Exercises(models.Model):
     sport = models.ForeignKey(Sports, on_delete=models.CASCADE)
@@ -32,12 +38,15 @@ class Exercises(models.Model):
     field3 = models.IntegerField(blank=True, null=True)
     field4 = models.IntegerField(blank=True, null=True)
     field5 = models.IntegerField(blank=True, null=True)
+    class Meta:
+        db_table = 'Exercises'  # Specify the table name explicitly
 
 class Unlocked(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
 
     class Meta:
+        db_table = 'Unlocked'  # Specify the table name explicitly
         unique_together = (('user', 'exercise'),)
 
 class Analytical(models.Model):
@@ -47,6 +56,8 @@ class Analytical(models.Model):
     toc = models.IntegerField()
     challenging = models.IntegerField()
     feedback = models.IntegerField()
+    class Meta:
+        db_table = 'Analytical'  # Specify the table name explicitly
 
 class Aggregated(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
@@ -59,5 +70,6 @@ class Aggregated(models.Model):
     CoT = models.IntegerField()
 
     class Meta:
+        db_table = 'Aggregated'  # Specify the table name explicitly
         unique_together = (('user', 'exercise'),)
  
