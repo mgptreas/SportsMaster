161a854e082bad8e76df6eb83e86dfc150b457ed
Marios Georgopetreas <95870493+mgptreas@users.noreply.github.com>
2024-06-04 20:11:14 +0300

    Home Screen

--- SportsMasterApp/app/src/main/java/com/example/sportsmasterapp/HomeScreenActivity.java
+++ SportsMasterApp/app/src/main/java/com/example/sportsmasterapp/HomeScreenActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     private ImageView ivUserIcon,ivLockIcon;
+    private LinearLayout llBasketball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         ivUserIcon = findViewById(R.id.iv_user_icon);
         ivLockIcon = findViewById(R.id.iv_lock_icon);
+        llBasketball = findViewById(R.id.ll_basketball);
 
         ivUserIcon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +34,15 @@ public class HomeScreenActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeScreenActivity.this, UnlockedExercisesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        llBasketball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreenActivity.this, FieldSelectActivity.class);
+                intent.putExtra("sport_name", "Basketball");
                 startActivity(intent);
             }
         });
